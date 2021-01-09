b3a08c3bf729678b1d6eee70d22769a23cc89543
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-01-09 23:21:29 +0400

    reviews and plan ended

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function () {
-    
-})
+$(document).ready(function () {
+    $('#reviews-items').slick({
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
