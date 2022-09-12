56a3840466a80b9c470286c66a0dbc8d37137570
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-09-12 22:02:24 +0400

    added listeners for click and scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,32 @@
-$(document).ready(function () {
-    $('#reviews-items').slick({
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
+const header = $("header");
+const sidebar = $("#sidebar");
+
+$(document).ready(() => {
+  $("#reviews-items").slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    centerMode: true,
+    adaptiveHeight: true,
+  });
+});
+
+$("#burger").on("click", () => {
+  if (sidebar.hasClass("active"))
+    return sidebar.removeClass("active").addClass("disable");
+
+  sidebar.removeClass("disable").addClass("active");
+});
+
+$("#close").on("click", () => {
+  sidebar.removeClass("active").addClass("disable");
+});
+
+$(document).on("scroll", () => {
+  console.log($(document).scrollTop());
+  if ($(document).scrollTop() > 0 && !header.hasClass("active")) {
+    header.addClass("active");
+  } else if ( $(document).scrollTop() === 0) {
+    header.removeClass("active");
+  }
 });
